db28d6eefe244d6ed70fc499ed592e5132ab003c
Klemen Kunstek <klemen.kunstek1@gmail.com>
2025-01-17 13:46:14 +0100

    chore(): remove unused files

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,16 +1,13 @@
-import Image from "next/image"
-import Link from "next/link"
-
-import { Announcement } from "@/components/announcement"
-import { CardsDemo } from "@/components/cards"
-import { ExamplesNav } from "@/components/examples-nav"
+import { Announcement } from "@/components/announcement";
 import {
   PageActions,
   PageHeader,
   PageHeaderDescription,
   PageHeaderHeading,
-} from "@/components/page-header"
-import { Button } from "@/registry/default/ui/button"
+} from "@/components/page-header";
+import { Button } from "@/registry/default/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function IndexPage() {
   return (
@@ -31,36 +28,6 @@ export default function IndexPage() {
           </Button>
         </PageActions>
       </PageHeader>
-      <div className="border-grid border-b">
-        <div className="container-wrapper">
-          <div className="container py-4">
-            <ExamplesNav className="[&>a:first-child]:text-primary" />
-          </div>
-        </div>
-      </div>
-      <div className="container-wrapper">
-        <div className="container py-6">
-          <section className="overflow-hidden rounded-lg border bg-background shadow-md md:hidden md:shadow-xl">
-            <Image
-              src="/examples/cards-light.png"
-              width={1280}
-              height={1214}
-              alt="Cards"
-              className="block dark:hidden"
-            />
-            <Image
-              src="/examples/cards-dark.png"
-              width={1280}
-              height={1214}
-              alt="Cards"
-              className="hidden dark:block"
-            />
-          </section>
-          <section className="hidden md:block [&>div]:p-0">
-            <CardsDemo />
-          </section>
-        </div>
-      </div>
     </>
-  )
+  );
 }
