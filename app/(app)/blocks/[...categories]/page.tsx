e7520cf4cb8d45dfa38926f0ad03d3b4cdeb214d
Didd <didd.tuni@akasha.world>
2025-01-27 20:32:48 +0300

    chore(): comment out rsc imports

--- app/(app)/blocks/[...categories]/page.tsx
+++ app/(app)/blocks/[...categories]/page.tsx
@@ -1,5 +1,5 @@
-import { getAllBlockIds } from "@/lib/blocks";
-import { BlockDisplay } from "@/components/block-display";
+// import { getAllBlockIds } from "@/lib/blocks";
+// import { BlockDisplay } from "@/components/block-display";
 import { registryCategories } from "@/registry/registry-categories";
 
 export const dynamicParams = false;
@@ -15,17 +15,19 @@ export default async function BlocksPage({
 }: {
   params: { categories?: string[] };
 }) {
-  const blocks = await getAllBlockIds(
-    ["registry:block"],
-    params.categories ?? []
-  );
+  // const blocks = await getAllBlockIds(
+  //   ["registry:block"],
+  //   params.categories ?? []
+  // );
 
-  return blocks.map((name) => (
-    <div
-      key={name}
-      className="border-grid container border-b py-8 first:pt-6 last:border-b-0 md:py-12"
-    >
-      <BlockDisplay name={name} />
-    </div>
-  ));
+  return <></>;
+
+  // blocks.map((name) => (
+  //   <div
+  //     key={name}
+  //     className="border-grid container border-b py-8 first:pt-6 last:border-b-0 md:py-12"
+  //   >
+  //     <BlockDisplay name={name} />
+  //   </div>
+  // ));
 }
