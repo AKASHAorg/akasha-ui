f398ee3b71b518174035a2a6115ad4ca0a1b12d5
Didd <didd.tuni@akasha.world>
2025-02-20 10:40:04 +0300

    chore():

--- registry/default/examples/extension-card-demo.tsx
+++ registry/default/examples/extension-card-demo.tsx
@@ -27,21 +27,21 @@ export default function ExtensionCardDemo() {
     <ExtensionCard>
       <ExtensionCardAvatar>
         <ExtensionCardAvatarImage
-          src="https://github.com/akashaorg.pngx"
+          src="https://github.com/akashaorg.png"
           alt="@akashaorg"
         />
         <ExtensionCardAvatarImage />
         <ExtensionCardAvatarFallback />
       </ExtensionCardAvatar>
       <ExtensionCardContent>
-        <ExtensionCardName>Antenna</ExtensionCardName>
+        <ExtensionCardName>Extensions</ExtensionCardName>
         <ProfileAvatarButton
           size="sm"
           profileDID="did:pkh:eip155:11155111:0x8a022905463998860516390fb27548479a098b95"
         >
           <ProfileAvatar>
             <ProfileAvatarImage
-              src="https://github.com/akashaorg.pngx"
+              src="https://github.com/akashaorg.png"
               alt="@akashaorg"
             />
             <ProfileAvatarFallback />
@@ -49,9 +49,7 @@ export default function ExtensionCardDemo() {
           <ProfileName>CoffeeLover</ProfileName>
           <ProfileDidField />
         </ProfileAvatarButton>
-        <ExtensionCardDescription>
-          Antenna is a social app with a global and customizable feed.
-        </ExtensionCardDescription>
+        <ExtensionCardDescription>App for extensions.</ExtensionCardDescription>
       </ExtensionCardContent>
       <ExtensionCardAction active={active}>
         <ExtensionCardActionInactive
