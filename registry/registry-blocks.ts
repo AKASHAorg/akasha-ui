7d2318e4fb14918f8a206e0fe4eaa7d3a1c18762
Klemen Kunstek <klemen.kunstek@akasha.world>
2025-02-28 15:35:16 +0100

    chore(): add page block, create post component

--- registry/registry-blocks.ts
+++ registry/registry-blocks.ts
@@ -2,35 +2,29 @@ import { Registry } from "@/registry/schema";
 
 export const blocks: Registry = [
   {
-    name: "sidebar-01",
+    name: "post-page",
     type: "registry:block",
     description: "A simple sidebar with navigation grouped by section.",
     registryDependencies: [
-      "sidebar",
-      "breadcrumb",
-      "separator",
-      "label",
-      "dropdown-menu",
+      "custom/profile-avatar-button",
+      "custom/card",
+      "custom/stack",
+      "custom/typography",
+      "custom/icon-container",
+      "custom/button",
     ],
+    dependencies: ["lucide-react"],
     files: [
       {
-        path: "blocks/sidebar-01/page.tsx",
+        path: "blocks/post-page/page.tsx",
         type: "registry:page",
         target: "app/dashboard/page.tsx",
       },
       {
-        path: "blocks/sidebar-01/components/app-sidebar.tsx",
-        type: "registry:component",
-      },
-      {
-        path: "blocks/sidebar-01/components/search-form.tsx",
-        type: "registry:component",
-      },
-      {
-        path: "blocks/sidebar-01/components/version-switcher.tsx",
+        path: "blocks/post-page/components/post.tsx",
         type: "registry:component",
       },
     ],
-    categories: ["sidebar"],
+    categories: ["post-page"],
   },
 ];
